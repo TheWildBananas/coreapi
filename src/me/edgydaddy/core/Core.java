8d1480af32f64526a2b0027219faaa1983adaa4b
EdgyDaddy <ME@PC.home>
2017-05-21 09:41:04 -0400

    Added code

---
+++ src/me/edgydaddy/core/Core.java
@@ -0,0 +1,109 @@
+package me.edgydaddy.core;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Core
+  extends JavaPlugin
+  implements Listener
+{
+  public static Core plugin;
+  
+  public void onEnable()
+  {
+    getLogger().info(getDescription().getName() + " has been enabled!");
+    getServer().getPluginManager().registerEvents(this, this);
+    getConfig().options().copyDefaults(true);
+    saveConfig();
+  }
+  
+  public void onDisable()
+  {
+    getLogger().info(getDescription().getName() + " has been disabled!");
+  }
+  
+  public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
+  {
+    Player player = (Player)sender;
+    if (commandLabel.equalsIgnoreCase("coreapi"))
+    {
+      player.sendMessage(ChatColor.RED + "WORKS");
+      return true;
+    }
+    return false;
+  }
+  
+  @EventHandler
+  public void onPlayerJoin(PlayerJoinEvent e)
+  {
+    Player p = e.getPlayer();
+    if (p.getName().equals(getConfig().getString("1")))
+    {
+      World world = getServer().getWorld("flatroom");
+      int x = getConfig().getInt("x");
+      int y = getConfig().getInt("y");
+      int z = getConfig().getInt("z");
+      world.getBlockAt(x, y, z).setType(Material.EMERALD_BLOCK);
+    }
+  }
+  
+  @EventHandler
+  public void onPlayerLeave(PlayerQuitEvent e)
+  {
+    Player p = e.getPlayer();
+    if (p.getName().equals("EdgyDaddy"))
+    {
+      World world = getServer().getWorld("flatroom");
+      int x = 0;
+      int y = 0;
+      int z = 0;
+      world.getBlockAt(x, y, z).setType(Material.REDSTONE_BLOCK);
+    }
+  }
+  
+  @EventHandler
+  public void onPlayerJoin1(PlayerJoinEvent e)
+  {
+    Player p = e.getPlayer();
+    if (p.getName().equals("ItsQuib"))
+    {
+      World world = getServer().getWorld("flatroom");
+      int x = 0;
+      int y = 0;
+      int z = 0;
+      world.getBlockAt(x, y, z).setType(Material.EMERALD_BLOCK);
+    }
+  }
+  
+  @EventHandler
+  public void onPlayerLeave1(PlayerQuitEvent e)
+  {
+    Player p = e.getPlayer();
+    if (p.getName().equals("ItsQuib"))
+    {
+      World world = getServer().getWorld("flatroom");
+      int x = 0;
+      int y = 0;
+      int z = 0;
+      world.getBlockAt(x, y, z).setType(Material.REDSTONE_BLOCK);
+    }
+  }
+  
+  @EventHandler
+  public void onDeath(PlayerDeathEvent e)
+  {
+    if (e.getEntity().getKiller() != null) {
+      e.getEntity().getWorld().strikeLightningEffect(e.getEntity().getLocation());
+    }
+  }
+}
