cb5ff2e6fe5e18418a362ac791a05500ffcdc191
ME <ME@PC.home>
2017-05-24 20:46:28 -0400

    Added Single User Config

--- src/me/edgydaddy/core/Core.java
+++ src/me/edgydaddy/core/Core.java
@@ -2,38 +2,47 @@ package me.edgydaddy.core;
 
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class Core extends JavaPlugin implements Listener {
+    FileConfiguration config = getConfig();
 
 	public void onEnable() {
+		super.onEnable();
 		getLogger().info(getDescription().getName() + " has been enabled!");
 		getServer().getPluginManager().registerEvents(this, this);
-		getConfig().options().copyDefaults(true);
-		saveConfig();
+		this.getConfig().options().copyDefaults(true);
+		this.saveConfig();
 	}
 
 	public void onDisable() {
 		getLogger().info(getDescription().getName() + " has been disabled!");
-
+		super.onDisable();
+		this.saveConfig();
 	}
-
+	
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent e) {
-		Player p = e.getPlayer();
-		if (p.getName().equals("EdgyDaddy")) {
-			World world = getServer().getWorld("flatroom");
-			int x = 99;
-			int y = 31;
-			int z = 700;
+		Player p =  e.getPlayer();
+		
+		if (config.getString("username") != null) {
+			World world = getServer().getWorld(config.getString("username.world"));
+			int x = Integer.parseInt(config.getString("username.x"));
+			int y = Integer.parseInt(config.getString("username.y"));
+			int z = Integer.parseInt(config.getString("username.z"));
 			world.getBlockAt(x, y, z).setType(Material.EMERALD_BLOCK);
-		} else if (p.getName().equals("EdgyDaddy")) {
+		}
+
+		else if (p.getName().equals("EdgyDaddy")) {
 			World world = getServer().getWorld("flatroom");
-			int x = 99;
+			int x = 91;
 			int y = 31;
 			int z = 700;
 			world.getBlockAt(x, y, z).setType(Material.REDSTONE_BLOCK);
@@ -41,7 +50,7 @@ public class Core extends JavaPlugin implements Listener {
 	}
 
 	@EventHandler
-	public void onPlayerJoin1(PlayerJoinEvent e) {
+	public void onPlayerJoin1(PlayerJoinEvent e) { 
 		Player p = e.getPlayer();
 		if (p.getName().equals("ItsQuib")) {
 			World world = getServer().getWorld("flatroom");
@@ -59,7 +68,7 @@ public class Core extends JavaPlugin implements Listener {
 			world.getBlockAt(x, y, z).setType(Material.REDSTONE_BLOCK);
 		}
 	}
-	
+	@EventHandler
 	public void onPlayerJoin2(PlayerJoinEvent e) {
 		Player p = e.getPlayer();
 		if (p.getName().equals("HomeWork")) {
@@ -78,6 +87,5 @@ public class Core extends JavaPlugin implements Listener {
 			world.getBlockAt(x, y, z).setType(Material.REDSTONE_BLOCK);
 		}
 	}
-
 
 }
